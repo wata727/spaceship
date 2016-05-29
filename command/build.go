ee97ca35b098fc7be8994b871b794f7b68dcfe4a
wata_mac <watassbass@gmail.com>
2016-05-30 02:20:35 +0900

    [WIP] creating build subcommand

--- command/build.go
+++ command/build.go
@@ -1,7 +1,11 @@
 package command
 
 import (
+	"os"
 	"strings"
+
+	client "github.com/wata727/spaceship/packer"
+	"github.com/wata727/spaceship/vui"
 )
 
 type BuildCommand struct {
@@ -10,6 +14,21 @@ type BuildCommand struct {
 
 func (c *BuildCommand) Run(args []string) int {
 	// Write your code here
+
+	pwd, _ := os.Getwd()
+	packer := &client.Packer{
+		Dir:      pwd + "/template",
+		Varfile:  "variables.json",
+		Template: "template.json",
+		Ui:       vui.Ui{},
+	}
+
+	c.Ui.Output("Building Artifact...")
+	if err := packer.Build(); err != nil {
+		c.Ui.Error("Failed packer build: " + err.Error())
+	}
+	c.Ui.Output(packer.Artifact.Output())
+	packer.Ui.Flush()
 
 	return 0
 }
